22fe1b4aab3c1e4103ba0bfb5f1812f61edccc6e
Zack Siri <me@zacksiri.dev>
2022-12-05 21:38:40 +0700

    update some fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'roboto-flex': ['Roboto Flex']
+			}
+		},
 	},
 	plugins: [
 		require('@tailwindcss/aspect-ratio')
