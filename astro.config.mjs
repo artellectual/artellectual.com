ac2ce5f1a73843f03edfdef0b523f4d52986f6ff
Zack Siri <me@zacksiri.dev>
2022-12-12 16:55:59 +0700

    Added site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  site: 'https://artellectual.com',
   integrations: [mdx(), tailwind(), react(), sitemap()],
   trailingSlash: 'always',
   adapter: cloudflare()
