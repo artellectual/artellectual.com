fee64c982bdf0c00decbabdb370c5111448088ff
Zack Siri <me@zacksiri.dev>
2022-12-12 17:13:50 +0700

    configure sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,18 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
+
+pages = ['/', '/services/', '/terms/', '/privacy/', '/about/']
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   site: 'https://artellectual.com',
-  integrations: [mdx(), tailwind(), react(), sitemap()],
-  trailingSlash: 'always'
+  integrations: [mdx(), tailwind(), react(), sitemap({
+   customPages: pages.map((page) => `https://artellectual.com/${page}`)
+  })],
+  trailingSlash: 'always',
+  adapter: cloudflare()
 });
