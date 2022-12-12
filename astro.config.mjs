301a6637078f3618e6ec52a8cbae5525d179a628
Zack Siri <me@zacksiri.dev>
2022-12-12 17:15:50 +0700

    fix sitemap const

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 
-pages = ['/', '/services/', '/terms/', '/privacy/', '/about/']
+const pages = ['/', '/services/', '/terms/', '/privacy/', '/about/']
 
 // https://astro.build/config
 export default defineConfig({
