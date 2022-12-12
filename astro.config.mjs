08037129de134b6bd1dabe8a6cd847356efbc12c
Zack Siri <me@zacksiri.dev>
2022-12-12 17:03:39 +0700

    switch to using static page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 import react from "@astrojs/react";
-
-// https://astro.build/config
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   site: 'https://artellectual.com',
   integrations: [mdx(), tailwind(), react(), sitemap()],
-  trailingSlash: 'always',
-  adapter: cloudflare()
+  trailingSlash: 'always'
 });
