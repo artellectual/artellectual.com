07b492f85cbde1ff4c75e2e1e958e0dfb606c2dc
Zack Siri <me@zacksiri.dev>
2024-01-29 22:39:48 +0700

    Update deps and fix build for static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,15 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 import react from "@astrojs/react";
-import cloudflare from "@astrojs/cloudflare";
+import node from '@astrojs/node';
 import sitemap from "@astrojs/sitemap";
 
 const pages = ['', 'services/', 'terms/', 'privacy/', 'about/']
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   site: 'https://artellectual.com',
   integrations: [mdx(), tailwind(), react(), sitemap({
    customPages: pages.map((page) => `https://artellectual.com/${page}`)
-  })],
-  trailingSlash: 'always',
-  adapter: cloudflare()
+  })]
 });
